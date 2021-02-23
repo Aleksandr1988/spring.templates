f0be8dcee5306ca0b27b4bad2f4769872612e440
Oleksandr Pashkov <brodpropertyprovans@gmail.com>
2021-02-23 14:13:04 +0200

    Add BeanFactoryPostProcessor

---
+++ bean.life.cycle/src/main/java/com/demonstration/spring/bean/life/cycle/beans/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package com.demonstration.spring.bean.life.cycle.beans;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomBeanFactoryPostProcessor.class);
+
+    @java.lang.Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        logger.info("--- BeanFactoryPostProcessor.postProcessBeanFactory - RUN");
+    }
+}
